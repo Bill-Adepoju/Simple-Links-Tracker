ab14403e0850c5aec9097f324a46147109d11807
unknown <wyze97@gmail.com>
2022-10-27 23:27:39 +0100

    Add Profile jsx Component

--- Dashboard task/src/App.jsx
+++ Dashboard task/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Profile from './components/Profile'
+import Links from './components/Links'
 
 function App() {
 
   return (
     <div className="App">
+      <Profile />
       
     </div>
   )
---
+++ Dashboard task/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import profileImg from "../images/profile-img.jpg"
+
+// This component contains the proile/header section 
+
+export default function profile(){
+    return(
+        <div className="profile-wrapper">
+            <img src={profileImg} alt="profile image" id="profile__img"/>
+            <div className="twitter-section">
+                <p id="twitter">twitter username: @iwillbegod_</p>
+            </div>
+            <div className="slack-section">
+                <p id="slack">slack username: William</p>
+            </div>
+        </div>
+    )
+}
