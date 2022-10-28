becc840e32c481b922bf59eaec7cc295724e8a7d
unknown <wyze97@gmail.com>
2022-10-28 14:10:04 +0100

    Add Necessary Links to the Links Component

---
+++ Dashboard task/src/components/Links.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Links(){
+    return(
+        <div className="links-wrapper">
+            <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
+            <a href="http://books.zuri.team/" id="books">Zuri Team</a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python">Python Books</a>
+            <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>
+            <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a>
+        </div>
+    )
+}
--- Dashboard task/src/components/Profile.jsx
+++ Dashboard task/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import profileImg from "../images/profile-img.jpg"
 
 // This component contains the proile/header section 
 
-export default function profile(){
+export default function Profile(){
     return(
         <div className="profile-wrapper">
             <img src={profileImg} alt="profile image" id="profile__img"/>
