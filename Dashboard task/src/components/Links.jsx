8570be224a8ceb972b02eddbe27b8a7c8480b8e7
unknown <wyze97@gmail.com>
2022-10-29 21:44:17 +0100

    Add some minimal stylying to the body

--- Dashboard task/src/components/Links.jsx
+++ Dashboard task/src/components/Links.jsx
@@ -1,13 +1,11 @@
-
-
 export default function Links(){
     return(
         <div className="links-wrapper">
-            <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
-            <a href="http://books.zuri.team/" id="books">Zuri Team</a>
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python">Python Books</a>
-            <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>
-            <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a>
+            <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a> <br />
+            <a href="http://books.zuri.team/" id="books">Zuri Team</a> <br />
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python">Python Books</a> <br />
+            <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>  <br />
+            <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a> <br />
         </div>
     )
 }
