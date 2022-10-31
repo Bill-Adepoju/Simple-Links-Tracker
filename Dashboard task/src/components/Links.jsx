c1649692c6246307e4796397e3ea2bcd1d6b14da
unknown <wyze97@gmail.com>
2022-10-31 12:22:14 +0100

    Ädd othrt breakpoints for mediaquery

--- Dashboard task/src/components/Links.jsx
+++ Dashboard task/src/components/Links.jsx
@@ -2,11 +2,11 @@ export default function Links(){
     return(
         <div className="links-wrapper">
             <a href="https://twitter.com/iwillbegod_" className="link-button">Twitter Link</a>
-            <a href="https://training.zuri.team/" id="btn__zuri" className="link-button">Zuri Team</a> <br />
-            <a href="http://books.zuri.team/" id="books" className="link-button">Zuri Team</a> <br />
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python" className="link-button">Python Books</a> <br />
-            <a href="https://background.zuri.team/" id="pitch" className="link-button">Background Check for Coders</a>  <br />
-            <a href="https://books.zuri.team/design-rules" id="book__design" className="link-button">Design Books</a> <br />
+            <a href="https://training.zuri.team/" id="btn__zuri" className="link-button">Zuri Team</a>
+            <a href="http://books.zuri.team/" id="books" className="link-button">Zuri Books</a> 
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python" className="link-button">Python Books</a>
+            <a href="https://background.zuri.team/" id="pitch" className="link-button">Background Check for Coders</a>  
+            <a href="https://books.zuri.team/design-rules" id="book__design" className="link-button">Design Books</a>
         </div>
     )
 }
