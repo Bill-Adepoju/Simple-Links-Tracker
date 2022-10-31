9b11eb4fe41bcf306490950123732e25a714cf3f
unknown <wyze97@gmail.com>
2022-10-31 12:38:39 +0100

    Correct some uncaught errors in instruction

--- Dashboard task/src/components/Links.jsx
+++ Dashboard task/src/components/Links.jsx
@@ -1,7 +1,7 @@
 export default function Links(){
     return(
         <div className="links-wrapper">
-            <a href="https://twitter.com/iwillbegod_" className="link-button">Twitter Link</a>
+            <a href="https://twitter.com/hnginternship" className="link-button">Twitter Link</a>
             <a href="https://training.zuri.team/" id="btn__zuri" className="link-button">Zuri Team</a>
             <a href="http://books.zuri.team/" id="books" className="link-button">Zuri Books</a> 
             <a href="https://books.zuri.team/python-for-beginners?ref_id=William" id="book__python" className="link-button">Python Books</a>
